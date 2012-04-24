fc5e11fab6531b8f3ac495bcfe48fc3887f9cb91
Michael Ramirez <ramirezmike2@gmail.com>
2012-04-24 19:43:53 -0400

    script can scrap all Books from search

--- test_one.py
+++ test_one.py
@@ -1,29 +1,36 @@
 import urllib2
 from BeautifulSoup import BeautifulSoup
 
-print "test"
 page_results_increment_by_ten = 0
-url = "http://santafe.bncollege.com/webapp/wcs/stores/servlet/BuyBackSearchCommand?extBuyBackSearchEnabled=Y&displayImage=N+&langId=-1&storeId=22566&catalogId=10001&isbn=&author=&title=%22+%22&start=" + str(page_results_increment_by_ten)
-req = urllib2.Request(url, headers={'User-Agent' : "Magic Browser"})
-con = urllib2.urlopen( req )
-print "url open"
-stringURL = con.read()
-con.close()
+total_books = 0
+#while (True):
+while (page_results_increment_by_ten < 10):
+		url = "http://santafe.bncollege.com/webapp/wcs/stores/servlet/BuyBackSearchCommand?extBuyBackSearchEnabled=Y&displayImage=N+&langId=-1&storeId=22566&catalogId=10001&isbn=&author=&title=%22+%22&start=" + str(page_results_increment_by_ten)
+		req = urllib2.Request(url, headers={'User-Agent' : "Magic Browser"})
+		con = urllib2.urlopen( req )
+		print "url open"
+		stringURL = con.read()
+		con.close()
 
-soup = BeautifulSoup(stringURL)
-print "soup made"
+		soup = BeautifulSoup(stringURL)
+		print "soup made"
 
-#print soup
+		#print soup
 
-count = 0
-for dd in soup.findAll("td"):
-	for td in dd.findAll("td"):
-		try:
-			if 'TITLE:' in td.text:
-				print "title found"
-				print td.text
-				count+=1
-		except:
-			print "no thread found"
+		count = 0
+		for dd in soup.findAll("td"):
+			for td in dd.findAll("td"):
+				try:
+					if 'TITLE:' in td.text:
+						print "title found"
+						print td.text
+						count+=1
+						total_books+=1
+				except:
+					print "no thread found"
+		page_results_increment_by_ten += 10
+		if (count < 10):
+			break
+		print count
 print "this is the end"
-print count
+print "total books equals:" + str(total_books)
