c162d582a7575afeda5a72e0ae380d1cac909d15
Michael Ramirez <ramirezmike2@gmail.com>
2012-05-09 18:26:50 -0400

    Adjusted def makeShortTitles; Increased results and decrease time of retry arrays + made more efficient

--- BAX.py
+++ BAX.py
@@ -85,13 +85,20 @@ def getRetryTitleArray(retryarray):
 def makeShortTitles(bookArray):
 	print "Making Short Book Titles..."
 	for book in bookArray:
-		regex = re.compile("\w+[^\W]")
-		match = regex.search(book.title)
-		if (match):
-			match = match.group()
-			book.title = match
-			print "Short title: " + book.title
+#		regex = re.compile("\w+[^\W]")
+#		match = regex.search(book.title)
+	#	if (match):
+	#		match = match.group()
+	#		book.title = match
+	#		print "Short title: " + book.title
+		while (len((book.title).split()) > 1):
+			string = re.sub("[^A-Za-z\d]"," ", book.title)	
+			string = string.split()
+			string.remove(string[len(string)/2])
+			string = " ".join(string)
+			book.title = string
 
+	
 # PRINTING DATA
 def printBook(book,number):
 	print "-" * 181
